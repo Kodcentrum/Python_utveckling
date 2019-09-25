d79789b9f24bb5c52d8608180ffdcfba40ed97b6
Rickard Löfberg <lofberg.rickard@gmail.com>
2019-09-25 21:06:02 +0200

    Abstraction of movement and objects
    - Introducing abstraction of movement class to move objects
      this is meant to represent the different movements you can do in
      Scratch. This should probably be its own module in future version.
    - Introduced Sprites instead to be able to support more objects.

--- main.py
+++ main.py
@@ -29,6 +29,32 @@ screen = pygame.display.set_mode(size)
 screen.fill(green)
 outercircle = pygame.draw.circle(screen,black,(300,300),outercircle_radius, tracksize)
 finish_line = pygame.draw.line(screen, white, (285, 170), (285, 20),3)
+
+class Bil(pygame.sprite.Sprite):
+    def __init__(self, surface, x_pos, y_pos):
+        super().__init__()
+
+        self.image = surface
+        self.rect = surface.get_rect()
+        self.rect.x = x_pos
+        self.rect.y = y_pos
+
+class Rörese:
+    def ändra_x_för(self, obj, x):
+        if hasattr(obj, 'rect'):
+            obj.rect.x += x
+
+    def ändra_y_för(self, obj, y):
+        if hasattr(obj, 'rect'):
+            obj.rect.y += y
+
+
+all_sprites = pygame.sprite.Group()
+ball_surface = pygame.image.load("bilder/ball.png")
+bil = Bil(ball_surface, x, y)
+all_sprites.add(bil)
+rörelse = Rörese()
+
 while 1:
     for event in pygame.event.get():
         # Closes window and program
@@ -40,19 +66,20 @@ while 1:
         trycka = KM(pygame.key.get_pressed())
 
         if trycka.på("vänsterpil"):
-            x -= 5
+            rörelse.ändra_x_för(bil, -5)
         if trycka.på("högerpil"):
-            x += 5
+            rörelse.ändra_x_för(bil, 5)
         if trycka.på("nedåtpil"):
-            y += 5
+            rörelse.ändra_y_för(bil, 5)
         if trycka.på("uppåtpil"):
-             y -= 5
+             rörelse.ändra_y_för(bil, -5)
+
         # Recreates background,grass, tracks without the cars previous position
         screen.fill(green)
         outercircle = pygame.draw.circle(screen, black, (300, 300), outercircle_radius, tracksize)
         finish_line = pygame.draw.line(screen, white, (285, 170), (285, 20),3)
         # Redraws the car on our new "clean" background with it's new coordinates!
-        car = pygame.draw.circle(screen, red, (x,y), radius_car)
+        all_sprites.draw(screen)
         pygame.display.update()
         # Checks if the car goes off the track, if yez then the race starts over.
         if (innercircle_radius > (abs(x-300)**2+abs(y-300)**2)**0.5) or (outercircle_radius < (abs(x-300)**2+abs(y-300)**2)**0.5):
